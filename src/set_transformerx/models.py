49aa1177a35ae94d2927b539cb14eec871e3f8c0
Shao-Ting Chiu <stchiu@email.tamu.edu>
2025-04-14 16:11:22 -0500

    update results

--- src/set_transformerx/models.py
+++ src/set_transformerx/models.py
@@ -6,19 +6,26 @@ from .modules import ISAB, SAB, PMA
 class SetTransformer(eqx.Module):
     enc: eqx.Module
     dec: eqx.Module
+    out: eqx.Module
     def __init__(self, dim_input, num_outputs, dim_output,
-            num_inds=32, dim_hidden=128, num_heads=4, ln=False, key=jr.PRNGKey(0)):
+            num_inds=32, dim_hidden=128, num_heads=4, ln=False, *, mlp_kwargs=None, key=jr.PRNGKey(0)):
         ks = jr.split(key, 6)
-        self.enc = eqx.nn.Sequential([
-                ISAB(dim_input, dim_hidden, num_heads, num_inds, ln=ln, key=ks[0]),
-                ISAB(dim_hidden, dim_hidden, num_heads, num_inds, ln=ln, key=ks[1])])
-        self.dec = eqx.nn.Sequential([
-                PMA(dim_hidden, num_heads, num_outputs, ln=ln, key=ks[2]),
-                SAB(dim_hidden, dim_hidden, num_heads, ln=ln, key=ks[3]),
-                SAB(dim_hidden, dim_hidden, num_heads, ln=ln, key=ks[4]),
-                jax.vmap(eqx.nn.Linear(dim_hidden, dim_output, key=ks[5]))])
+        self.enc = [
+                ISAB(dim_input, dim_hidden, num_heads, num_inds, ln=ln, mlp_kwargs=mlp_kwargs, key=ks[0]),
+                ISAB(dim_hidden, dim_hidden, num_heads, num_inds, ln=ln, mlp_kwargs=mlp_kwargs, key=ks[1])]
+        self.dec = [
+                PMA(dim_hidden, num_heads, num_outputs, ln=ln, mlp_kwargs=mlp_kwargs, key=ks[2]),
+                SAB(dim_hidden, dim_hidden, num_heads, ln=ln, mlp_kwargs=mlp_kwargs, key=ks[3]),
+                SAB(dim_hidden, dim_hidden, num_heads, ln=ln, mlp_kwargs=mlp_kwargs, key=ks[4])]
+        
+        self.out = eqx.nn.Linear(dim_hidden, dim_output, key=ks[5])
 
     def __call__(self, X):
-        X = self.enc(X)
+        for b in self.enc:
+                X = b(X)
+        for b in self.dnc:
+                X = b(X)
         X = self.dec(X)
+        X = jax.vmap(self.out)(X)
         return X
+
