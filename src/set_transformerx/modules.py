50f790944c57346d4c47382170807f31ba9a4038
Shao-Ting Chiu <stchiu@email.tamu.edu>
2025-04-14 14:09:00 -0500

    add mlp arg

--- src/set_transformerx/modules.py
+++ src/set_transformerx/modules.py
@@ -5,7 +5,7 @@ import equinox as eqx
 import jax
 import jax.numpy as jnp
 from jax import random as jr
-from typing import Callable
+from typing import Callable, Optional
 
 class MAB(eqx.Module):
     fc_q: eqx.nn.Linear 
@@ -17,7 +17,7 @@ class MAB(eqx.Module):
     dim_V: int 
     num_heads: int
 
-    def __init__(self, dim_Q, dim_K, dim_V, num_heads:int, ln=False, key=jr.PRNGKey(0)):
+    def __init__(self, dim_Q, dim_K, dim_V, num_heads:int, *, ln=False, mlp_kwargs: Optional[dict]=None, key=jr.PRNGKey(0), **kwargs):
         """
         ln: layernorm
         """
@@ -35,35 +35,43 @@ class MAB(eqx.Module):
             self.res1 = lambda x: x
             self.res2 = lambda x: x
 
-        self.fc_o = eqx.nn.Linear(dim_V, dim_V, key=k[3])
+        if mlp_kwargs is None:
+            mlp_kwargs = dict(
+                activation=jax.nn.gelu, 
+                width_size=None,
+                depth=0
+            )
+
+        self.fc_o = eqx.nn.MLP(in_size=dim_V, out_size=dim_V, **mlp_kwargs,key=k[3])
 
     def __call__(self, Q, K, **kwargs):
-        Q = jax.vmap(self.fc_q)(Q)
-        K, V = jax.vmap(self.fc_k)(K), jax.vmap(self.fc_qv)(K)
-        Q = jnp.expand_dims(Q, axis=0)
-        K = jnp.expand_dims(K, axis=0)
-        V = jnp.expand_dims(V, axis=0)
+        q = jax.vmap(self.fc_q)(Q)
+        k = jax.vmap(self.fc_k)(K)
+        v = jax.vmap(self.fc_qv)(K)
+        q = jnp.expand_dims(q, axis=0)
+        k = jnp.expand_dims(k, axis=0)
+        v = jnp.expand_dims(v, axis=0)
 
 
         dim_split = self.dim_V // self.num_heads
-        Q_ = jnp.concatenate(jnp.split(Q, dim_split, axis=2), axis=0)
-        K_ = jnp.concatenate(jnp.split(K, dim_split, axis=2), axis=0)
-        V_ = jnp.concatenate(jnp.split(V, dim_split, axis=2), axis=0)
-
+        Q_ = jnp.concatenate(jnp.split(q, dim_split, axis=2), axis=0)
+        K_ = jnp.concatenate(jnp.split(k, dim_split, axis=2), axis=0)
+        V_ = jnp.concatenate(jnp.split(v, dim_split, axis=2), axis=0)
 
         A = jax.nn.softmax(jax.lax.batch_matmul(Q_, K_.transpose(0,2,1))) / jnp.sqrt(self.dim_V)
         
         O = (Q_ + jax.lax.batch_matmul(A, V_)).reshape(-1,self.dim_V)
         
+        # Residual connection
         O = self.res1(O)
-        O = O + jax.nn.relu(jax.vmap(self.fc_o)(O))
+        O = O + jax.nn.gelu(jax.vmap(self.fc_o)(O))
         O = self.res2(O)
         return O
 
 class SAB(eqx.Module):
     mab: MAB
-    def __init__(self, dim_in, dim_out, num_heads, ln=False, key=jr.PRNGKey(0)):
-        self.mab = MAB(dim_in, dim_in, dim_out, num_heads, ln=ln, key=key)
+    def __init__(self, dim_in, dim_out, num_heads, ln=False, *, mlp_kwargs:Optional[dict]=None, key=jr.PRNGKey(0), **kwargs):
+        self.mab = MAB(dim_in, dim_in, dim_out, num_heads, ln=ln, key=key, mlp_kwargs=mlp_kwargs, **kwargs)
     def __call__(self, X, **kwargs):
         return self.mab(X,X)
 
@@ -71,12 +79,12 @@ class ISAB(eqx.Module):
     I: jnp.ndarray
     mab0: MAB
     mab1: MAB
-    def __init__(self, dim_in, dim_out, num_heads, num_inds, ln=False, key=jr.PRNGKey(0)):
+    def __init__(self, dim_in, dim_out, num_heads, num_inds, ln=False,*, mlp_kwargs:Optional[dict]=None, key=jr.PRNGKey(0), **kwargs):
         ks = jr.split(key, 2)
         init = jax.nn.initializers.glorot_uniform()
         self.I = init(key, (num_inds, dim_out)) 
-        self.mab0 = MAB(dim_out, dim_in, dim_out, num_heads, ln=ln, key=ks[0]) 
-        self.mab1 = MAB(dim_in, dim_out, dim_out, num_heads, ln=ln, key=ks[1])
+        self.mab0 = MAB(dim_out, dim_in, dim_out, num_heads, ln=ln, key=ks[0], mlp_kwargs=mlp_kwargs, **kwargs) 
+        self.mab1 = MAB(dim_in, dim_out, dim_out, num_heads, ln=ln, key=ks[1], mlp_kwargs=mlp_kwargs, **kwargs)
     def __call__(self, X, **kwargs):
         H = self.mab0(self.I,X)
         return self.mab1(X,H)
@@ -84,11 +92,11 @@ class ISAB(eqx.Module):
 class PMA(eqx.Module):
     S: jnp.ndarray
     mab: MAB
-    def __init__(self, dim, num_heads, num_seeds, ln=False, key=jr.PRNGKey(0)):
+    def __init__(self, dim, num_heads, num_seeds, ln=False, *, mlp_kwargs:Optional[dict]=None, key=jr.PRNGKey(0), **kwargs):
         ks = jr.split(key, 2)
         init = jax.nn.initializers.glorot_uniform()
         self.S = init(ks[0], (num_seeds, dim))
-        self.mab = MAB(dim, dim, dim, num_heads, ln=ln, key=ks[1])
+        self.mab = MAB(dim, dim, dim, num_heads, ln=ln, mlp_kwargs=mlp_kwargs, key=ks[1], **kwargs)
 
     def __call__(self, X, **kwargs):
         H = self.mab(self.S,X)
